0bb3173530036c459a76860c1253194ecda517d8
Sujay <sujaydey641@gmail.com>
2024-03-05 22:43:46 +0530

    Setup jenkins in ubuntu
    Signed-off-by: Sujay <sujaydey641@gmail.com>

---
+++ setup-jenkins-ubuntu.sh
@@ -0,0 +1,9 @@
+RUN sudo apt-get update
+RUN sudo apt install openjdk-11-jre -y
+RUN sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+RUN echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+RUN sudo apt-get update
+RUN sudo apt-get install jenkins -y
